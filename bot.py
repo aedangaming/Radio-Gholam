67dd8eaa3a6bafdd365bd322e8a93c4626077c51
m-ehsan <m_ehsan_sh@live.com>
2023-08-01 21:39:46 +0330

    used minimal intetnts required

--- bot.py
+++ bot.py
@@ -15,7 +15,8 @@ MAX_IDLE_SECONDS = float(os.getenv("MAX_IDLE_SECONDS"))
 PREFERRED_PROXIES = json.loads(os.getenv("PREFERRED_PROXIES"))
 
 
-intents = nextcord.Intents.all()
+intents = nextcord.Intents.default()
+intents.message_content = True
 client = commands.Bot(command_prefix="!", intents=intents)
 
 
